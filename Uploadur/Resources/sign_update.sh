898c642bb2e7e091a791fc1c2ab74e01e2082c52
Tassilo Selover-Stephan <Tassilo@selover.net>
2014-09-22 09:59:31 +0200

    streamlining update signing

--- Uploadur/Resources/sign_update.sh
+++ Uploadur/Resources/sign_update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 set -o pipefail
-if [ "$#" -ne 2 ]; then
+if [ "$#" -ne 1 ]; then
   echo "Usage: $0 update_archive private_key"
   exit 1
 fi
 openssl=/usr/bin/openssl
-$openssl dgst -sha1 -binary < "$1" | $openssl dgst -dss1 -sign "$2" | $openssl enc -base64
+$openssl dgst -sha1 -binary < "webhosting assets/Binaries/Uploadur-$1.zip" | $openssl dgst -dss1 -sign "/Users/tassilo/Google Drive/Dev/Uploadur/dsa_priv.pem" | $openssl enc -base64
